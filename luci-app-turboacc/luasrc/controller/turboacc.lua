67653ca3efe2c349d138d61cfa101b76a030c22b
沉默の金 <110812055+chenmozhijin@users.noreply.github.com>
2024-02-20 17:29:12 +0800

    Attempt to fix #19

--- luci-app-turboacc/luasrc/controller/turboacc.lua
+++ luci-app-turboacc/luasrc/controller/turboacc.lua
@@ -13,7 +13,7 @@ function index()
 end
 
 local function fastpath_status()
-	return luci.sys.call("/etc/init.d/turboacc check_status fastpath") == 0
+	return luci.sys.call("/etc/init.d/turboacc check_status fastpath 0") == 0
 end
 
 local function bbr_status()
